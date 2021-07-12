595b45f975f3196fdbe173be1aff50d53560a50a
lilin7 <36003947+lilin7@users.noreply.github.com>
2021-07-12 10:33:38 -0400

    a prettier HomeScreen
    finish video 6

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import products from '../products'
 
 const Product = ({product}) => {
     return (
@@ -7,8 +8,25 @@ const Product = ({product}) => {
             <a href={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top'></Card.Img>
             </a>
-            
+
+            <Card.Body>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
+                </a>
+
+                <Card.Text as='div'>
+                    <div className='my-3'>{product.rating} from {product.numReviews} reviews</div>
+                </Card.Text>
+
+                <Card.Text as='h3'>
+                    ${product.price}
+                </Card.Text>
+
+            </Card.Body>
+
         </Card>
+
+        
     )
 }
 
