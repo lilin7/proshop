b269c1d404194bfa2702409d7e80667c3d90addb
lilin7 <36003947+lilin7@users.noreply.github.com>
2021-07-19 18:29:19 -0400

    add get request and reply result from server (home, all products, single product page)

--- backend/data/products.js
+++ backend/data/products.js
@@ -79,4 +79,5 @@ const products = [
   },
 ]
 
-export default products
+// export default products //this is es module
+module.exports = products //common js, for server to use
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
 // entry point for server
+
 const express = require('express') //common js
+const products = require('./data/products')
 
 const app = express()
+
+// to be able to see text in server http://localhost:5000
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+//to get all products info (in json)
+//e.g. http://localhost:5000/api/products
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+//to get a single product info (in json)
+//e.g. http://localhost:5000/api/products/1
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p=>p._id === req.params.id)
+    res.json(product)
+})
 
 app.listen(5000, console.log("Server running on port 5000"))
 
