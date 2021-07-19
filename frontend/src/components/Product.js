90ef151b3d2ba7288087e5bd99bc0a48e5b9c6fd
lilin7 <36003947+lilin7@users.noreply.github.com>
2021-07-19 16:29:42 -0400

    Add product screen (of individual product when click from homepage), link cart and login (remove reloading)

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 import products from '../products'
 import Rating from './Rating'
@@ -6,14 +7,14 @@ import Rating from './Rating'
 const Product = ({product}) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top'></Card.Img>
-            </a>
+            </Link>
 
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
-                </a>
+                </Link>
 
                 <Card.Text as='div'>
                     {/* <div className='my-3'>{product.rating} from {product.numReviews} reviews</div> */}
