5f8145eec369e1d19083d04ea34df481c63185e5
lilin7 <36003947+lilin7@users.noreply.github.com>
2021-07-19 17:41:33 -0400

    add content in single product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button  } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products' 
 
-const ProductScreen = () => {
+
+const ProductScreen = ( {match} ) => {
+    const product = products.find((p) => p._id === match.params.id)
+
+
     return (
-        <div>
-            Product
-        </div>
+        <>
+            <Link className='btn btn-light my-3' to='/'>Go Back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid></Image>
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>
+                                {product.name}
+                            </h2>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`}></Rating>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: ${product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: ${product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </>
+
     )
 }
 
