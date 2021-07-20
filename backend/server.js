9ffbfa2cd6cf26db8a21428548b9ce6f9784be90
lilin7 <36003947+lilin7@users.noreply.github.com>
2021-07-19 21:56:46 -0400

    add color to console print

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -36,6 +37,6 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000 //use dotenv to import environment variable, if not defined, use 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 
