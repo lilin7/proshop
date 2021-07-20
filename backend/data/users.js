69ddcc0ff3910b1f1eea68971375d84af028c2cf
lilin7 <36003947+lilin7@users.noreply.github.com>
2021-07-20 00:03:40 -0400

    connect MongoDB (empty), create data in there
    has not fetched data from MongoDB

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs' //to hash password
+
+const users = [
+    {
+      name: 'Admin User',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456', 10),
+      isAdmin: true,
+    },
+    {
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: bcrypt.hashSync('123456', 10),
+    },
+    {
+      name: 'Jane Doe',
+      email: 'jane@example.com',
+      password: bcrypt.hashSync('123456', 10),
+    },
+  ]
+  
+  export default users
